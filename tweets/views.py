887a8f1571d251117d5d948d0f9d459f90a2de03
ironfalcon <ironfalcon@yandex.ru>
2020-12-05 13:20:06 -0800

    (pure js) work on form error msg handler

--- tweets/views.py
+++ tweets/views.py
@@ -14,6 +14,7 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    # print(abc)
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
