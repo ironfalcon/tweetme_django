d1e4833067e0dfbea3f33f9f51ac03a8caa1474b
ironfalcon <ironfalcon@yandex.ru>
2020-12-06 13:19:06 -0800

    link user to tweets in model

--- tweets/models.py
+++ tweets/models.py
@@ -1,13 +1,20 @@
 import random
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='image/', blank=True, null=True)
 
+    # def __str__(self):
+    #     return self.content
+    
     class Meta:
         ordering = ["-id"]
+    
     def serialize(self):
         return {
             "id": self.id,
